d0f082d433d7daee0707ecef865d9cdb4e0bbacb
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-11 12:35:41 +0530

    Added: Reading a single-digit no. and printing name in word

---
+++ src/main/java/com/bridgelabz/day2/SingleDigit.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.day2;
+
+import java.util.Scanner;
+
+public class SingleDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = scanner.nextInt();
+        if(n<0)
+            System.out.println("Enter valid single digit no");
+        else
+            if(n==0)
+                System.out.println("Zero");
+            else
+                if(n==1)
+                    System.out.println("One");
+                else
+                    if (n==2)
+                        System.out.println("Two");
+                    else
+                        if (n==3)
+                            System.out.println("Three");
+                        else
+                            if (n==4)
+                                System.out.println("Four");
+                            else
+                                if (n==5)
+                                    System.out.println("Five");
+                                else
+                                    if (n==6)
+                                        System.out.println("Six");
+                                    else
+                                        if (n==7)
+                                            System.out.println("Seven");
+                                        else
+                                            if (n==8)
+                                                System.out.println("Eight");
+                                            else
+                                                System.out.println("Nine");
+    }
+}
